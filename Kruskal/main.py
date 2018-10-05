4361ac60568d3255a13275b3278e623623098de2
Valfredo Santos Júnior <valfredojr3@gmail.com>
2018-10-05 11:01:11 -0300

    Adição da classe Aresta
    Implementei uma classe Aresta para usar como a estrutura para guardar os dados. É melhor do que a versão anterior que usava o dicionário, pois não preciso mais ficar utilizando o método split().

--- Kruskal/main.py
+++ Kruskal/main.py
@@ -1,6 +1,14 @@
-''' Realiza a leitura do arquivo e retorna um dicionário com as arestas e seus pesos,
-    uma lista da ordem crescente das arestas em relação ao peso e uma lista que informa 
-    em qual árvore cada vértice se encontra inicialmente. '''
+class Aresta:    
+    def __init__(self, vertice1, vertice2, valor):
+        self.vertice1 = vertice1
+        self.vertice2 = vertice2
+        self.valor = valor
+    
+    def toString(self):
+        return ('(' + str(self.vertice1) + ',' + str(self.vertice2) + ')')
+
+def takeValue(elem):
+    return elem.valor
 
 def ReadFile(filename):
     with open(filename, 'r') as file:    
@@ -8,22 +16,24 @@ def ReadFile(filename):
         input = aux.split('\n')
         input = [x for x in input if x != '']
                 
-        edges = {}        
+        edges = []        
         n = input.pop(0)
         
         aux = 0;
         for i in range(int(n)):
             for j in range (i+1, int(n)):
-                edges[str(i) + ',' + str(j)] = int(input[aux])
+                edges.append(Aresta(i, j, int(input[aux])))
                 aux += 1
         
-        sequenceList = sorted(edges, key = edges.get)
+        
+        edges.sort(key = takeValue)
+        
         treetable = []
         
         for x in range(int(n)):
             treetable.append(x)
         
-        return edges, sequenceList, treetable
+        return edges, treetable
 
 def main():
     result = []
@@ -40,26 +50,30 @@ def main():
     option = input('>: ')
     print('\nTeste escolhido: ' + tests.get(option))
     
-    edges, sequenceList, treetable = ReadFile(tests.get(option))
+    edges, treetable = ReadFile(tests.get(option))
     
-    for x in sequenceList:
+    for x in edges:
+                           
+        tree1 = treetable[x.vertice1]
+        tree2 = treetable[x.vertice2]
         
-        aux = x.split(',')
-        
-        if (treetable[int(aux[0])] != treetable[int(aux[1])]): # Checa se os vértices não se encontram na mesma árvore
+        if (tree1 != tree2): # Checa se os vértices não se encontram na mesma árvore
             #print ("Current Solution:", result)
             result.append(x)
-            resultValue += edges.get(x)
-            
-            tree1 = treetable[int(aux[0])]
-            tree2 = treetable[int(aux[1])]
+            resultValue += x.valor
             
             for y in range(len(treetable)): # Atualiza a àrvore de todos os vértices que foram unidos
                 if (treetable[y] == tree2):
                     treetable[y] = tree1
         
-    print ("\nFinal Solution:", result)
-    print ("Solution's Value:", resultValue)
+    print ('Solution:', end='')
+    
+    for x in result:
+        print(x.toString(), end='')
+    
+    print('')
+        
+    print ('Value:', resultValue)
             
 if __name__ == '__main__':
     main()
